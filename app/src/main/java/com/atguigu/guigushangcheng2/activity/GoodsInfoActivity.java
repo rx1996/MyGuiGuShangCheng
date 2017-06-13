12abc4eb25092eae3bcdaf6662732686ac627142
rx1996 <3343108599@qq.com>
2017-06-13 14:58:43 +0800

    十七 :    设置商品详情页面数据

--- app/src/main/java/com/atguigu/guigushangcheng2/activity/GoodsInfoActivity.java
+++ app/src/main/java/com/atguigu/guigushangcheng2/activity/GoodsInfoActivity.java
@@ -1,8 +1,13 @@
 package com.atguigu.guigushangcheng2.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,9 +19,12 @@ import android.widget.Toast;
 import com.atguigu.guigushangcheng2.R;
 import com.atguigu.guigushangcheng2.adapter.HomeAdapter;
 import com.atguigu.guigushangcheng2.bean.GoodsBean;
+import com.atguigu.guigushangcheng2.utils.Constants;
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GoodsInfoActivity extends AppCompatActivity {
 
@@ -62,6 +70,7 @@ public class GoodsInfoActivity extends AppCompatActivity {
     LinearLayout llRoot;
     @BindView(R.id.activity_goods_info)
     LinearLayout activityGoodsInfo;
+    private GoodsBean goodsBean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,10 +78,102 @@ public class GoodsInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_goods_info);
         ButterKnife.bind(this);
         getData();
+        setData();
+    }
+
+    private void setData() {
+        //设置图片和名字和价格的数据
+        String coverPrice = goodsBean.getCover_price();
+        String figure = goodsBean.getFigure();//图片地址
+        String productId = goodsBean.getProduct_id();//商品id
+        String name = goodsBean.getName();//名称
+
+        //设置图片
+        Glide.with(this).load(Constants.BASE_URL_IMAGE + figure).into(ivGoodInfoImage);
+        tvGoodInfoName.setText(name);
+        tvGoodInfoPrice.setText("￥" + coverPrice);
+
+
+        //设置webView的数据
+        setWebViewData(productId);
+    }
+
+    private void setWebViewData(String productId) {
+        WebSettings webSettings = wbGoodInfoMore.getSettings();
+        //设置支持js
+        webSettings.setJavaScriptEnabled(true);
+        //设置支持双击变大变小
+        webSettings.setUseWideViewPort(true);
+
+        //设置检索缓存的
+        webSettings.setCacheMode(webSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        //设置不跳转到系统的浏览器
+        wbGoodInfoMore.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    view.loadUrl(request.getUrl().toString());
+                }
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
+        wbGoodInfoMore.loadUrl("http://mp.weixin.qq.com/s/Cf3DrW2lnlb-w4wYaxOEZg");
     }
 
     private void getData() {
-        GoodsBean goodsBean = (GoodsBean) getIntent().getSerializableExtra(HomeAdapter.GOODS_BEAN);
-        Toast.makeText(this, ""+goodsBean.toString(), Toast.LENGTH_SHORT).show();
+        goodsBean = (GoodsBean) getIntent().getSerializableExtra(HomeAdapter.GOODS_BEAN);
+//        Toast.makeText(this, "" + goodsBean.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick({R.id.ib_good_info_back, R.id.ib_good_info_more, R.id.tv_good_info_callcenter, R.id.tv_good_info_collection, R.id.tv_good_info_cart, R.id.btn_good_info_addcart, R.id.tv_more_share, R.id.tv_more_search, R.id.tv_more_home, R.id.btn_more})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ib_good_info_back:
+                finish();
+                break;
+            case R.id.ib_good_info_more:
+                llRoot.setVisibility(View.VISIBLE);
+                break;
+            case R.id.tv_good_info_callcenter:
+                Toast.makeText(GoodsInfoActivity.this, "客服中心", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_good_info_collection:
+                Toast.makeText(GoodsInfoActivity.this, "收藏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_good_info_cart:
+                Toast.makeText(GoodsInfoActivity.this, "进入购物车", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_good_info_addcart:
+                Toast.makeText(GoodsInfoActivity.this, "添加购物车", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_more_share:
+                Toast.makeText(GoodsInfoActivity.this, "分享", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_more_search:
+                Toast.makeText(GoodsInfoActivity.this, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_more_home:
+                Toast.makeText(GoodsInfoActivity.this, "主页", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_more:
+                llRoot.setVisibility(View.GONE);
+                break;
+        }
     }
 }
