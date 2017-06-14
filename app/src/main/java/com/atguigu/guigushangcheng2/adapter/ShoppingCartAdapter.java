e596175443da0ca0a2daab6c40c2b5a0264fb229
rx1996 <3343108599@qq.com>
2017-06-14 18:45:30 +0800

    二十七 :   全选和反选

--- app/src/main/java/com/atguigu/guigushangcheng2/adapter/ShoppingCartAdapter.java
+++ app/src/main/java/com/atguigu/guigushangcheng2/adapter/ShoppingCartAdapter.java
@@ -123,6 +123,20 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
             });
         }
     }
+    public void checkAll_none(boolean isCheck) {
+        if(datas != null && datas.size() >0){
+
+            for(int i = 0; i < datas.size(); i++) {
+                GoodsBean goodsBean = datas.get(i);
+                //只要有一个不选中就设置非全选
+                goodsBean.setCheck(isCheck);
+                notifyItemChanged(i);
+            }
+        }else{
+            checkboxAll.setChecked(false);
+        }
+
+    }
 
     private void checkAll() {
         if(datas != null && datas.size() >0){
