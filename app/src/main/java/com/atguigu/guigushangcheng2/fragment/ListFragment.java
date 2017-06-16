d12f949d51505d8b111d236af534f8aeb659fa20
rx1996 <3343108599@qq.com>
2017-06-16 19:50:34 +0800

    三十五 :    联网请求数据

--- app/src/main/java/com/atguigu/guigushangcheng2/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/guigushangcheng2/fragment/ListFragment.java
@@ -2,25 +2,34 @@ package com.atguigu.guigushangcheng2.fragment;
 
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.guigushangcheng2.R;
 import com.atguigu.guigushangcheng2.adapter.TypeLeftAdapter;
 import com.atguigu.guigushangcheng2.basefragment.BaseFragment;
+import com.atguigu.guigushangcheng2.bean.TypeBean;
+import com.atguigu.guigushangcheng2.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.Callback;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.Call;
 
 /**
  * Created by Administrator on 2017/6/16.
  */
 
 public class ListFragment extends BaseFragment {
+    private static final String TAG = ListFragment.class.getSimpleName();
     @BindView(R.id.lv_left)
     ListView lvLeft;
     @BindView(R.id.rv_right)
@@ -30,6 +39,9 @@ public class ListFragment extends BaseFragment {
     private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
             "办公文具", "数码周边", "游戏专区"};
 
+    private String[] urls = new String[]{Constants.SKIRT_URL, Constants.JACKET_URL, Constants.PANTS_URL, Constants.OVERCOAT_URL,
+            Constants.ACCESSORY_URL, Constants.BAG_URL, Constants.DRESS_UP_URL, Constants.HOME_PRODUCTS_URL, Constants.STATIONERY_URL,
+            Constants.DIGIT_URL, Constants.GAME_URL};
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.fragment_list, null);
@@ -50,11 +62,42 @@ public class ListFragment extends BaseFragment {
                 typeLeftAdapter.changeSelectPosition(position);
                 //2.适配器刷新
                 typeLeftAdapter.notifyDataSetChanged();//getView
+                //联网请求
+                getDataFromNet(urls[position]);
             }
         });
-
+        getDataFromNet(urls[0]);
     }
 
+    private void getDataFromNet(String url) {
+        System.out.println("url=="+url);
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+    class MyStringCallback extends StringCallback {
+
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            Log.e(TAG, "请求成功失败==" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e(TAG, "请求成功=="+response);
+            processData(response);
+
+        }
+    }
+
+    private void processData(String json) {
+        TypeBean typeBean = JSON.parseObject(json,TypeBean.class);
+
+        Log.e("TAG","解析成功=="+ typeBean.getResult().get(0).getName());
+    }
 
     @Override
     public void onDestroyView() {
