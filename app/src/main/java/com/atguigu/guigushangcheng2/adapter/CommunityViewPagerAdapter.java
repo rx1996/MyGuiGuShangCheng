8dcdfe347ba0f466b9c5ec3c703a43a8212ec01f
rx1996 <3343108599@qq.com>
2017-06-17 15:50:34 +0800

    三十九 :   发现的布局文件和实例化

---
+++ app/src/main/java/com/atguigu/guigushangcheng2/adapter/CommunityViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.atguigu.guigushangcheng2.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.atguigu.guigushangcheng2.basefragment.BaseFragment;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017/6/17.
+ */
+
+public class CommunityViewPagerAdapter extends FragmentPagerAdapter {
+    private final ArrayList<BaseFragment> fragments;
+
+    public CommunityViewPagerAdapter(FragmentManager fm, ArrayList<BaseFragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+    /**
+     * 根据位置得到对应的Fragment
+     * @param position
+     * @return
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
---
+++ app/src/main/java/com/atguigu/guigushangcheng2/fragment/HotPostFragment.java
@@ -0,0 +1,35 @@
+package com.atguigu.guigushangcheng2.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.guigushangcheng2.basefragment.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/6/17.
+ */
+
+public class HotPostFragment extends BaseFragment {
+    private TextView textView;
+
+    /**
+     * 初始化控件
+     * retur
+     */
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("我是热帖内容");
+    }
+}
